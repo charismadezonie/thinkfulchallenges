91e143e3f9a0ef010b8383cc91f8c43ac04bb79e
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-19 19:05:52 -0500

    move file to correct place

--- Project_Flashcards_Qualified_1/src/Layout/Home.js
+++ Project_Flashcards_Qualified_1/src/Layout/Home.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { Card } from "react";
 
 function Home() {
-  return <p>Hey you home</p>;
+  return (
+    <>
+      <button type="button">Create Deck</button>
+      <Card>
+        <p>Here's a card</p>
+        <button type="button">View</button>
+        <button type="button">Study</button>
+      </Card>
+    </>
+  );
 }
 
 export default Home;
