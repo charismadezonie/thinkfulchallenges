441fbbc961f1369010c9fd3bbeb5653dc27ee5e0
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-24 09:05:45 -0500

    sample code

--- React_Mock_Interview-Thaddeus_Daniels-Solution/src/App.js
+++ React_Mock_Interview-Thaddeus_Daniels-Solution/src/App.js
@@ -3,22 +3,23 @@ import PostDetail from "./PostDetail";
 
 function App() {
   const [posts, setPosts] = useState([]);
-  
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts?userId=1")
       .then((response) => response.json())
       .then(setPosts)
       .catch((error) => {
-        console.log(error)
+        console.log(error);
       });
   }, []);
- 
-  
+
   return (
     <div className="App">
-      <PostDetail posts={posts}/>
+      <PostDetail posts={posts} />
     </div>
   );
 }
 
 export default App;
+
+// this is a whole bunch of nonsense stuff
