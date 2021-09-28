963570360e615d0cc7c30cab8fbf114a16dfa297
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-28 09:58:46 -0500

    starter files

--- React_Mock_Interview-Thaddeus_Daniels-Solution/src/PostDetail.js
+++ React_Mock_Interview-Thaddeus_Daniels-Solution/src/PostDetail.js
@@ -3,13 +3,15 @@ import React, { useState, useEffect } from "react";
 function PostDetail({ posts }) {
   const [comments, setComments] = useState([]);
   const [clicked, setClicked] = useState(false);
+  const [postId, setPostId] = useState(1);
 
-  const handleClick = () => {
+  const handleClick = ({ id }) => {
+    setPostId(id);
     setClicked(!clicked);
   };
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/posts?userId=1`)
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
       .then((response) => response.json())
       .then(setComments)
       .catch((error) => {
@@ -22,10 +24,12 @@ function PostDetail({ posts }) {
       {posts.map(({ title, body, id }) => (
         <div>
           <p>{title}</p>
-          <p id={id} onClick={handleClick}>
+          <p id={id} onClick={() => handleClick({ id })}>
             {body}
           </p>
-          {clicked ? <p>{comments[0].body}</p> : null}
+          {comments.map((comment) =>
+            clicked && id === postId ? <p>{comment.body}</p> : null
+          )}
         </div>
       ))}
     </div>
