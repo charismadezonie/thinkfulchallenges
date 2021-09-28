6505b373a61e6ce5c927b14d62bed1544688adeb
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-28 11:32:22 -0500

    reset files

--- Node_Express_Project_Configuration_1/src/app.js
+++ Node_Express_Project_Configuration_1/src/app.js
@@ -1,16 +1,4 @@
 const express = require("express");
 const app = express();
 
-const sayHello = (req, res, next) => {
-  res.send("Hello!");
-};
-
-const logging = (req, res, next) => {
-  console.log("A request is being made!");
-  next();
-};
-
-app.use(logging);
-app.use(sayHello);
-
 module.exports = app;
