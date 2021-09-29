2eb0cbc0dfaf75156319dc2be50765137131b6f5
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-29 12:02:39 -0500

    /check/:zip route

--- Node_Express_Assignment_1/src/app.js
+++ Node_Express_Assignment_1/src/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const validateZip = require("./middleware/validateZip");
+const getZoos = require("./utils/getZoos");
 const app = express();
 
 app.get("/check/:zip", validateZip, (req, res, next) => {
-  res.send(`Enjoy your trip to ${req.params.zip}!`);
+  const zip = req.params.zip;
+  getZoos(zip)
+    ? res.send(`${zip} exists in our records.`)
+    : res.send(`${zip} does not exist in our records.`);
 });
 
-app.get("/zoos/:all", validateZip, (req, res, next) => {
+app.get("/zoos/all", (req, res, next) => {
   res.send(`Enjoy your trip to ${req.params.all}!`);
 });
 
@@ -15,7 +19,7 @@ app.get("/zoos/:zip", validateZip, (req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.send(`The route ${req.path} does not exist!`);
+  res.send("That route could not be found!");
 });
 
 app.use((err, req, res, next) => {
