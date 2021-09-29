2a71eefac402bfa74c24739ff802a2be194fc682
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-29 11:38:47 -0500

    add route structures that use validateZip

--- Node_Express_Assignment_1/src/app.js
+++ Node_Express_Assignment_1/src/app.js
@@ -1,27 +1,18 @@
 const express = require("express");
+const validateZip = require("./middleware/validateZip");
 const app = express();
 
-app.get(
-  "/travel/:abbreviation",
-  checkForAbbreviationLength,
-  (req, res, next) => {
-    res.send(`Enjoy your trip to ${req.params.abbreviation}!`);
-  }
-);
-app.get(
-  "/travel/:abbreviation",
-  checkForAbbreviationLength,
-  (req, res, next) => {
-    res.send(`Enjoy your trip to ${req.params.abbreviation}!`);
-  }
-);
-app.get(
-  "/travel/:abbreviation",
-  checkForAbbreviationLength,
-  (req, res, next) => {
-    res.send(`Enjoy your trip to ${req.params.abbreviation}!`);
-  }
-);
+app.get("/check/:zip", validateZip, (req, res, next) => {
+  res.send(`Enjoy your trip to ${req.params.abbreviation}!`);
+});
+
+app.get("/zoos/:all", validateZip, (req, res, next) => {
+  res.send(`Enjoy your trip to ${req.params.abbreviation}!`);
+});
+
+app.get("/zoos/:zip", validateZip, (req, res, next) => {
+  res.send(`Enjoy your trip to ${req.params.abbreviation}!`);
+});
 
 app.use((req, res, next) => {
   res.send(`The route ${req.path} does not exist!`);
