0a10ea25256785a7b081a921f95587486ec2fa40
thaddeusrayd <thaddeusrayd@gmail.com>
2021-09-30 13:24:03 -0500

    error fallbacks

--- Robust_Server_Structure_Static_data_1/src/app.js
+++ Robust_Server_Structure_Static_data_1/src/app.js
@@ -5,6 +5,9 @@ const users = require("./data/users-data");
 const states = require("./data/states-data");
 
 // TODO: return an array of users from /users in form of { data: Array }
+app.use("/users", (req, res) => {
+  res.send("hey");
+});
 
 // TODO: return a single user by id from /users/:userId in form of { data: Object }
 
@@ -13,7 +16,13 @@ const states = require("./data/states-data");
 // TODO: Return a single state from /states/:stateCode in the form of { data: { stateCode: String, name: String } }
 
 // TODO: add not found handler
+app.use((request, response, next) => {
+  next(`Not found: ${request.originalUrl}`);
+});
 
 // TODO: Add error handler
+app.use((error, request, response, next) => {
+  response.send(error);
+});
 
 module.exports = app;
