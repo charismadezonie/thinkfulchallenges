63014d43c0f884f8406a0e40b747825e5214826e
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-05 11:21:58 -0500

    requirements met - note: TESTS WILL NOT PASS UNLESS IN QUALIFIED - this commit does not connect to database locally

---
+++ PostgreSQL_Inserting_Data_1/src/artists/insert-multiple-artists.sql
@@ -0,0 +1,6 @@
+INSERT INTO artists
+  (first_name, last_name, birthday, is_alive)
+  VALUES
+  ('Something', 'Rembrandt', '1993-05-15', false),
+  ('Pablo', 'Picasso', '1993-05-15', false),
+  ('Salvador', 'Dali', '1993-05-15', false)
