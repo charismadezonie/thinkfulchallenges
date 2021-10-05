88d9d81d730ef7ce8c76250adc1e968102f21684
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-04 19:14:27 -0500

    connect to db

--- PostgreSQL_Creating_Dropping_Databases_1/src/solution.js
+++ PostgreSQL_Creating_Dropping_Databases_1/src/solution.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: "",
-  user: "",
-  database: "",
-  password: "",
+  host: "fanny.db.elephantsql.com",
+  user: "tpjfjnnp",
+  database: "tpjfjnnp",
+  password: "5-v8nVYELZLpg_Po6QodAtivqqTj4_0B",
 };
