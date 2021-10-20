9c39ffffa17ee9316008d1de185914e4b914ad64
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-20 12:27:50 -0500

    build out theaters.router

---
+++ Module 39 - Project - WeLoveMovies/src/theaters/theaters.router.js
@@ -0,0 +1,7 @@
+const router = require("express");
+const controller = require("./reviews.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+router.route("/").get(controller.list);
+
+module.exports = router;
