8f77a41fbefeb94d5ab8b5605d8df79bcb0b0c6d
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-20 11:23:31 -0500

    add update func and reviewExists middleware to reviews.controller

--- Module 39 - Project - WeLoveMovies/src/reviews/reviews.controller.js
+++ Module 39 - Project - WeLoveMovies/src/reviews/reviews.controller.js
@@ -1,4 +1,30 @@
 const reviewsService = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-module.exports = {};
+function reviewExists(req, res, next) {
+  reviewsService
+    .read(req.params.reviewId)
+    .then((review) => {
+      if (review) {
+        res.locals.review = review;
+        return next();
+      }
+      next({ status: 404, message: `Review cannot be found.` });
+    })
+    .catch(next);
+}
+
+function update(req, res, next) {
+  const updatedReview = {
+    ...req.body.data,
+    review_id: res.locals.review.review_id,
+  };
+  reviewsService
+    .update(updatedReview)
+    .then((data) => res.json({ data }))
+    .catch(next);
+}
+
+module.exports = {
+  update: [reviewExists, update],
+};
