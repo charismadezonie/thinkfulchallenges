7b16040c8422f84063973d0c649ab83816109081
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-20 11:19:34 -0500

    add read and update funcs to reviews.service

--- Module 39 - Project - WeLoveMovies/src/reviews/reviews.service.js
+++ Module 39 - Project - WeLoveMovies/src/reviews/reviews.service.js
@@ -1,3 +1,18 @@
 const knex = require("../db/connection");
 
-module.exports = {};
+function read(review_id) {
+  return knex("reviews").select("*").where({ review_id }).first();
+}
+
+function update(updatedReview) {
+  return knex("suppliers")
+    .select("*")
+    .where({ review_id: updatedReview.review_id })
+    .update(updatedReview, "*")
+    .first();
+}
+
+module.exports = {
+  read,
+  update,
+};
