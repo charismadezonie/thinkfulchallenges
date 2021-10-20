11f2154565878595a39699cfa108a9cc0382a06b
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-20 12:40:53 -0500

    finish up theaters.router

--- Module 39 - Project - WeLoveMovies/src/theaters/theaters.controller.js
+++ Module 39 - Project - WeLoveMovies/src/theaters/theaters.controller.js
@@ -1,4 +1,9 @@
 const theatersService = require("./theaters.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-module.exports = {};
+async function list(req, res) {
+  const theaters = await theatersService.list();
+  res.status(200).json({ data: theaters });
+}
+
+module.exports = { list };
