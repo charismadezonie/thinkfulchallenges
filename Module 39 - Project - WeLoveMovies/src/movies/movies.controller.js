d25dee191aa92c2b42b9e08b48b7c194745f9950
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-20 09:44:02 -0500

    fit addCritic into a loop; all movies routes working

--- Module 39 - Project - WeLoveMovies/src/movies/movies.controller.js
+++ Module 39 - Project - WeLoveMovies/src/movies/movies.controller.js
@@ -29,7 +29,22 @@ function movieExists(req, res, next) {
     .catch(next);
 }
 
+async function getTheaters(req, res) {
+  const data = await moviesService.getTheaters(res.locals.movie.movie_id);
+  res.json({ data });
+}
+
+async function getReviews(req, res) {
+  const data = await moviesService.getReviews(res.locals.movie.movie_id);
+  res.json({ data });
+}
+
 module.exports = {
   list: [asyncErrorBoundary(list)],
   read: [movieExists, read],
+  getTheaters: [
+    asyncErrorBoundary(movieExists),
+    asyncErrorBoundary(getTheaters),
+  ],
+  getReviews: [asyncErrorBoundary(movieExists), asyncErrorBoundary(getReviews)],
 };
--- Module 39 - Project - WeLoveMovies/src/movies/movies.router.js
+++ Module 39 - Project - WeLoveMovies/src/movies/movies.router.js
@@ -3,6 +3,14 @@ const { get } = require("request");
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
+router
+  .route("/:movieId/reviews")
+  .get(controller.getReviews)
+  .all(methodNotAllowed);
+router
+  .route("/:movieId/theaters")
+  .get(controller.getTheaters)
+  .all(methodNotAllowed);
 router.route("/:movieId").get(controller.read).all(methodNotAllowed);
 router.route("/").get(controller.list).all(methodNotAllowed);
 
