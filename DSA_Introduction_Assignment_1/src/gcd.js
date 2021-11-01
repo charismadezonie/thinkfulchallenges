4f279b7f7f5af864638b5ddaee7751d06d2879eb
thaddeusrayd <thaddeusrayd@gmail.com>
2021-11-01 13:24:55 -0500

    algorithm 1 functionality

--- DSA_Introduction_Assignment_1/src/gcd.js
+++ DSA_Introduction_Assignment_1/src/gcd.js
@@ -2,8 +2,21 @@
  * Write a function named gcd1 that implements algorithm 1 here
  */
 
+function gcd1(a, b) {
+  if (a === 0) return b;
+  if (b === 0) return a;
+  while (b !== 0) {
+    let r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
+}
+
 /**
  * Write a function named gcd2 that implements algorithm 2 here
  */
+
+function gcd2(a, b) {}
 
 module.exports = { gcd1, gcd2 };
