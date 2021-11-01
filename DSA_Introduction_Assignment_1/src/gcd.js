44e0869666e67271104cb73b1dc1f90f81c0cb66
thaddeusrayd <thaddeusrayd@gmail.com>
2021-11-01 13:31:19 -0500

    more tests passing for algorithm 2

--- DSA_Introduction_Assignment_1/src/gcd.js
+++ DSA_Introduction_Assignment_1/src/gcd.js
@@ -17,6 +17,34 @@ function gcd1(a, b) {
  * Write a function named gcd2 that implements algorithm 2 here
  */
 
-function gcd2(a, b) {}
+function gcd2(a, b) {
+  if (a === 0) return b;
+  if (b === 0) return a;
+  let divA = [1, a];
+  let divB = [1, b];
+  let common = [];
+  for (let i = 2; i < a.length; i++) {
+    if (a % i === 0) {
+      divA.push(i);
+    }
+  }
+  for (let i = 2; i < a.length; i++) {
+    if (b % i === 0) {
+      divB.push(i);
+    }
+  }
+  divA.forEach((n) => {
+    if (divB.includes(n)) {
+      common.push(n);
+    }
+  });
+  let largest = common[0];
+  common.forEach((n) => {
+    if (n > largest) {
+      largest = n;
+    }
+  });
+  return largest;
+}
 
 module.exports = { gcd1, gcd2 };
