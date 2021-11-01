59acbcdfe2d8572158e6cc45e37964a1c6387616
thaddeusrayd <thaddeusrayd@gmail.com>
2021-11-01 11:34:50 -0500

    all tests passing

--- DSA_Introduction_Algorithm_Efficiency_1/src/cable.js
+++ DSA_Introduction_Algorithm_Efficiency_1/src/cable.js
@@ -3,7 +3,23 @@
  Implement algorithm 1 here
 */
 
+exports.cable1 = (currentLength, requiredLength, saleLength) => {
+  let count = 0;
+  let i = currentLength;
+  while (i < requiredLength) {
+    i += saleLength;
+    count++;
+  }
+  return count;
+};
+
 /**
  The cable problem
  Implement algorithm 2 here
 */
+
+exports.cable2 = (currentLength, requiredLength, saleLength) => {
+  const distance = requiredLength - currentLength;
+  let total = Math.round(distance / saleLength);
+  return total;
+};
