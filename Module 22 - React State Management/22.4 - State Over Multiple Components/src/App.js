010e7efa384b9249f76874545b71ec219e99dc36
thaddeusrayd <thaddeusrayd@gmail.com>
2021-11-12 09:26:41 -0600

    add 22.4

---
+++ Module 22 - React State Management/22.4 - State Over Multiple Components/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Content from "./Content";
+import Header from "./Header";
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [fontSize, setFontSize] = useState(12);
+  const toggleLoggedIn = () => setLoggedIn(!loggedIn);
+  const increaseFontSize = () => setFontSize(fontSize + 2);
+  return (
+    <div>
+      <Header
+        loggedIn={loggedIn}
+        handleLoggedInClick={toggleLoggedIn}
+        handleFontSizeClick={increaseFontSize}
+      />
+      <Content loggedIn={loggedIn} fontSize={fontSize} />
+    </div>
+  );
+}
+
+export default App;
