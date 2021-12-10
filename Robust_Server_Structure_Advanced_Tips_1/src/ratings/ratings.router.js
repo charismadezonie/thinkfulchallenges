6f174b923db06517aba1703e1aeb82c9a9cc67fe
Thaddeus Daniels <thaddeusrayd@gmail.com>
2021-12-10 13:09:22 -0600

    Delete Robust_Server_Structure_Advanced_Tips_1 directory

--- Robust_Server_Structure_Advanced_Tips_1/src/ratings/ratings.router.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router({ mergeParams: true });
-const controller = require("./ratings.controller");
-const methodNotAllowed = require("../errors/methodNotAllowed");
-
-router.route("/:ratingId").get(controller.read).all(methodNotAllowed);
-router.route("/").get(controller.list).all(methodNotAllowed);
-
-module.exports = router;
