2b43ceef61f6d11f14a37eb82bf1b319fd98e8f9
thaddeusrayd <thaddeusrayd@gmail.com>
2021-12-16 13:51:04 -0600

    add 33.9

---
+++ Module 33 - Postgres/33.9 - Assessment/src/artists-insert-multiple.sql
@@ -0,0 +1,27 @@
+INSERT
+  INTO 
+  artists (artist_name, birthday, birthplace, is_alive)
+VALUES
+  (
+   'Pablo Picasso', 
+   '1993-05-17', 
+   'Paris, France', 
+   false
+  ),
+  (
+   'Thad Daniels', 
+   '1993-05-17', 
+   'Flowood, USA', 
+   true
+  ),
+  (
+   'Banksy', 
+   '1993-05-17', 
+   'London, UK', 
+   true),
+  (
+   'Kara Stanley',
+   '1992-05-05',
+   'Huntsville, AL',
+   true
+  )
