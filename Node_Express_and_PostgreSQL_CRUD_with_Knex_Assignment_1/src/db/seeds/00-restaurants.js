a443555da23b0d2394826971817c56b758e12ce3
Thaddeus Daniels <thaddeusrayd@gmail.com>
2021-12-16 14:41:29 -0600

    Delete Node_Express_and_PostgreSQL_CRUD_with_Knex_Assignment_1 directory

--- Node_Express_and_PostgreSQL_CRUD_with_Knex_Assignment_1/src/db/seeds/00-restaurants.js
+++
@@ -1,7 +0,0 @@
-const restaurants = require("./00-restaurants.json");
-
-exports.seed = function (knex) {
-  return knex
-    .raw("TRUNCATE TABLE restaurants RESTART IDENTITY CASCADE")
-    .then(() => knex("restaurants").insert(restaurants));
-};
