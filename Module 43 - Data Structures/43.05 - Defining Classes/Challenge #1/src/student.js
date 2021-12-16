97ffb02fb881c741de683b807bd905fbeef5547f
thaddeusrayd <thaddeusrayd@gmail.com>
2021-12-16 14:31:47 -0600

    add 43.5 challenges

---
+++ Module 43 - Data Structures/43.05 - Defining Classes/Challenge #1/src/student.js
@@ -0,0 +1,19 @@
+class Student {
+  constructor(
+    firstName,
+    lastName,
+    birthYear,
+    birthMonth,
+    birthDay, 
+    gender) 
+  {
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.birthYear = birthYear,
+    this.birthMonth = birthMonth,
+    this.birthDay = birthDay,
+    this.gender = gender
+  }
+}
+
+module.exports = Student;
